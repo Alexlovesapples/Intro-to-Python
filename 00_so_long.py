59e49c7033c58d74251af1d15bad4e08ba656a5b
Alexlovesapples <41022037+Alexlovesapples@users.noreply.github.com>
2018-07-10 14:34:03 -0700

    Create 00_so_long.py

---
+++ 00_so_long.py
@@ -0,0 +1,25 @@
+animal_plural = "sharks"
+emotion = "sad"
+character_trait = "stupidity"
+adjective = "fat"
+animal_2 = "hippo"
+
+print("So long and thanks for all the",animal_plural,)
+print("So" ,emotion, "that it should come to this")
+print("We tried to warn you all but oh dear?")
+print("might explain your disrespect")
+print("For all the" ,adjective, "wonders that")
+print("grow around you")
+print("So long, so long and thanks")
+print("for all the" ,animal_plural,)
+print("The world’s about to be destroyed")
+print("There’s no point getting all" ,emotion,)
+print("Lie back and let the planet dissolve")
+print("Despite those nets of" ,animal_2, "fleet")
+print("We thought that most of you were" ,emotion,)
+print("Especially" ,adjective, "tots and your")
+print("pregnant women")
+print("So long, so long, so long, so long, so long")
+print("So long, so long, so long, so long, so long")
+print("So long, so long and thanks")
+print("for all the" ,animal_plural,)
