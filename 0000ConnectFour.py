a69c48e2bc01fed9f6e9bdb5231a65503cbf24f0
Alexlovesapples <41022037+Alexlovesapples@users.noreply.github.com>
2018-07-19 13:50:47 -0700

    Add files via upload

---
+++ 0000ConnectFour.py
@@ -0,0 +1,418 @@
+import sys
+a = "."
+b = "."
+c = "."
+d = "."
+e = "."
+f = "."
+g = "."
+h = "."
+i = "."
+j = "."
+k = "."
+l = "."
+m = "."
+n = "."
+o = "."
+p = "."
+q = "."
+r = "."
+s = "."
+t = "."
+u = "."
+v = "."
+w = "."
+x = "."
+y = "."
+z = "."
+a1 = "."
+b2 = "."
+c3 = "."
+d4 = "."
+e5 = "."
+f6 = "."
+g7 = "."
+h8 = "."
+i9 = "."
+j10 = "."
+k11 = "."
+l12 = "."
+m13 = "."
+n14 = "."
+o15 = "."
+p16 = "."
+print("0  1  2  3  4  5  6")
+print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+blyat = input("Player one's turn: ")
+if blyat == "0":
+	j10 = "X"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "1":
+	k11 = "X"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "2":
+	l12 = "X"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "3":
+	m13 = "X"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "4":
+	n14 = "X"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "5":
+	o15 = "X"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "6":
+	p16 = "X"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+else:
+	print("Good job messing up the simplest of things")
+	sys.exit()
+blyat = input("Player two's turn: ")
+if blyat == "0" and j10 == "X":
+	c3 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "0" and j10 == ".":
+	j10 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "1" and k11 == "X":
+	d4 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "1" and k11 == ".":
+	k11 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "2" and l12 == "X":
+	e5 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "2" and l12 == ".":
+	l12 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "3" and m13 == "X":
+	f6 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "3" and m13 == ".":
+	m13 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "4" and n14 == "X":
+	g7 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "4" and n14 == ".":
+	n14  = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "5" and o15 == "X":
+	h8 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "5" and o15 == ".":
+	o15 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "6" and p16 == "X":
+	i9 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "6" and p16 == ".":
+	p16 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+else:
+	print("Good job messing up the simplest of things")
+	sys.exit()
+blyat = input("Player one's turn")
+if blyat == "0" and j10 == "X" or j10 == "O" and c3 == "X" or c3 == "O" and v == ".":
+	v = "X"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "0" and j10 == "X" or j10 == "O" and c3 == ".":
+	c3 = "X"
+elif blyat == "0" and j10 == ".":
+	j10 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "1" and k11 == "X":
+	d4 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "1" and k11 == ".":
+	k11 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "2" and l12 == "X":
+	e5 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "2" and l12 == ".":
+	l12 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "3" and m13 == "X":
+	f6 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "3" and m13 == ".":
+	m13 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "4" and n14 == "X":
+	g7 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "4" and n14 == ".":
+	n14  = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "5" and o15 == "X":
+	h8 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "5" and o15 == ".":
+	o15 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "6" and p16 == "X":
+	i9 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+elif blyat == "6" and p16 == ".":
+	p16 = "O"
+	print("0  1  2  3  4  5  6")
+	print("%s  %s  %s  %s  %s  %s  %s" % (a, b, c, d, e, f, g))
+	print("%s  %s  %s  %s  %s  %s  %s" % (h, i, j, k, l, m, n))
+	print("%s  %s  %s  %s  %s  %s  %s" % (o, p, q, r, s, t, u))
+	print("%s  %s  %s  %s  %s  %s  %s" % (v, w, x, y, z, a1, b2))
+	print("%s  %s  %s  %s  %s  %s  %s" % (c3, d4, e5, f6, g7, h8, i9))
+	print("%s  %s  %s  %s  %s  %s  %s" % (j10, k11, l12, m13, n14, o15, p16))
+	del blyat
+else:
+	print("Good job messing up the simplest of things")
+	sys.exit()
+
+	'''
+	this code is absolute garbage, ima scrap this
+	'''
